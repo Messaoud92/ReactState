6fa707d20ade074aeb515b4607110d29b2e2c35c
Messaoud92 <messaoud.souid@gmail.com>
2022-11-28 12:02:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [show, setshow] = useState(false);
+  const [person, setperson] = useState({
+    name:"Messaoud Souid" , bio: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. "
+    ,imgSrc :"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg", profession:"Developper" 
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button  onClick={()=>setshow(!show)}> click here to show profil </button>
+  
+    {show?
+     <div>
+      <h1> {person.name}</h1>
+      <h1> {person.bio}</h1>
+      <h1> {person.profession}</h1>
+      <img src={person.imgSrc}/>
+      </div>
+    : null}   
     </div>
   );
 }
